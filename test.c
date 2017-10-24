9ce69550d058c945200943678db47f9d57504763
Christian Jaeger <ch@christianjaeger.ch>
2017-10-24 03:45:59 +0100

    test: add screen_write_pgm, write images to t/

--- test.c
+++ test.c
@@ -14,6 +14,48 @@
 #include "utils.h"
 
 
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void die_errno(const char* msg, const char* arg) {
+	fprintf(stderr, "error: %s (%s): %s\n", msg, arg, strerror(errno));
+	exit(1);
+}
+
+
+// Write screen buffer format from Nokia5110.c to file in PGM
+// format. Take dimensions from Nokia5110.h
+static void screen_write_pgm(const char* screen, const char* path) {
+	int w= SCREENW;
+	int h= SCREENH;
+
+	FILE *fh= fopen(path, "w");
+	if (!fh) die_errno("open", path);
+
+#define PRINTF(...) if(fprintf(fh, __VA_ARGS__) < 0) die_errno("print", path)
+
+	PRINTF("P2\n");
+	PRINTF("# CREATOR: SpaceInvaders test\n");
+	PRINTF("%d %d\n", w, h);
+
+	for (int y=0; y<h; y++) {
+		for (int x=0; x<w; x++) {
+			int i= x + w*(y/8);
+			char v= screen[i];
+			PRINTF("%d\n", ((v >> (y%8)) & 1) ? 0 : 255);
+		}
+	}
+	
+#undef PRINTF
+	
+	if (fclose(fh)) die_errno("close", path);
+}
+
+
+
+
 static void game_step() {
 	SysTick_Handler();
 	main_update_LCD();
@@ -35,12 +77,18 @@ int main () {
 	Random_Init(1);
 
 	REPEAT(1, game_step());
-	GPIO_PORTE_DATA_R=1;
+	screen_write_pgm(Screen, "t/1");
 	
-	REPEAT(8, game_step());
+	GPIO_PORTE_DATA_R=1;
+	REPEAT(10-1, game_step());
+	screen_write_pgm(Screen, "t/10");
+	
+	GPIO_PORTE_DATA_R=1;
+	REPEAT(40-8-1, game_step());
+	screen_write_pgm(Screen, "t/40");
 
 	// look at Nokia buffer.
-	trap();
+	//trap();
 	
 	return 0;
 }
