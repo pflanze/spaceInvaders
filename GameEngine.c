02e52d27670ec4018b53e635c4ba57f9297a05ce
Christian Jaeger <ch@christianjaeger.ch>
2020-07-31 14:13:46 +0100

    GameEngine: make definitions that don't need to be public private

--- GameEngine.c
+++ GameEngine.c
@@ -22,6 +22,70 @@
 #include "Sound.h"
 
 
+
+//----------------------------------Definitions---------------------------------
+//game max
+#define MAXLASERS 5
+#define MAX_ENEMY_PR 4
+#define REAL_MAX_EPR (MAX_ENEMY_PR-1)
+#define ALLOC_MAXROWS	2
+
+//used for Convert2Distance calibration
+#define Avalue 16
+#define Bvalue 0
+
+//collision offset
+#define YOFFSET 6
+#define XOFFSET 2
+
+//screen specs
+#define LOWERLIMIT 47
+#define RIGHTLIMIT 68
+#define LEFTLIMIT 0
+#define TOPLIMIT 6
+
+//Ship collision line
+
+#define SHIPCOLLISIONLINE LOWERLIMIT-PLAYERH+2
+
+//ship midPoints
+#define SHIPMIDDLE	((PLAYERW>>1)-1)
+
+//BonusShip
+#define BONUS_C_LINE TOPLIMIT+ENEMYBONUSH
+#define BONUSTIMING 30
+#define OFFSETEXPLOSIONX 2
+#define OFFSETEXPLOSIONY 4
+
+//BonusShip midPoint xCoordinate
+#define ENEMY_Bon_midX (ENEMYBONUSW>>1)
+
+//enemy midPoint xCoordinate
+#define E_LASER_OFFX ((ENEMY30W>>1)-1)
+#define E_LASER_OFFY 5
+
+//Objecdt IDs
+#define ID_SHIP			0
+#define ID_ENEMY		1
+#define ID_BONUS		2
+#define ID_E_LASER	3
+#define ID_S_LASER	4
+
+
+//Default values
+#define	FIRST_E	0
+#define	LAST_E	3
+
+//Return vs update (mode)
+#define RETURNVAL	0
+#define UPDATE		1
+#define RETURNARR	3
+
+//Miselaneus
+#define NA 1
+
+
+
 struct GameStatColumn {
 	unsigned char Fep;		//"First enemy position"
 	unsigned char Epc;		//"Enemies per column"
