1ce8308d6a97b97193696bc60685e7ea4d1ce5de
Christian Jaeger <ch@christianjaeger.ch>
2020-09-17 16:29:36 +0000

    CMWC: try to fix the PRNG, apparently it's just initialization

--- src/CMWC.c
+++ src/CMWC.c
@@ -3,16 +3,10 @@
 */
 
 #include <string.h>
+#include <stdio.h>
+
 
 unsigned long CMWC_Q[4096], CMWC_c=123, CMWC_i=4095;
-
-void CMWC_init(unsigned long seed) {
-	for (int i=0; i<4096; i++) {
-		CMWC_Q[i]= seed;
-	}
-	CMWC_c=123;
-	CMWC_i=4095;
-}
 
 unsigned long CMWC(void)
 {
@@ -20,6 +14,7 @@ unsigned long CMWC(void)
 	unsigned long x, m=0xFFFFFFFE;
 
 	CMWC_i = (CMWC_i + 1) & 4095;
+	printf("CMWC_c=%lu, CMWC_i=%lu\n", CMWC_c, CMWC_i);
 	t = a * CMWC_Q[CMWC_i] + CMWC_c;
 	CMWC_c = (t >> 32);
 	x = t + CMWC_c;
@@ -31,4 +26,22 @@ unsigned long CMWC(void)
 	return (CMWC_Q[CMWC_i] = m - x);
 }
 
+void CMWC_init(unsigned long seed) {
+	for (unsigned long i=0; i<4096; i++) {
+		CMWC_Q[i]= seed + i;
+	}
+	CMWC_c=123;
+	CMWC_i=4095;
+
+	// try to properly mix up the state before use (yes, I *really* have no
+	// idea of cryptography):
+	unsigned long _i = 0;
+	for (unsigned long i=0; i<4096; i++) {
+		CMWC();
+		CMWC_Q[i] ^= (seed - i);
+		_i = (_i + i + CMWC_Q[_i]) & 4095;
+		printf("i=%lu, _i=%lu, CMWC_Q[i]=%lu\n", i, _i, CMWC_Q[i]);
+		CMWC_Q[_i] ^= CMWC_Q[i];
+	}
+}
 
