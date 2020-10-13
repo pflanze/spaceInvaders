43cf1205baa6a41f1e218c0018e3a72b1cd45d83
Christian Jaeger <ch@christianjaeger.ch>
2020-10-13 22:11:10 +0000

    test.c: generate md5sums.txt

--- src/test.c
+++ src/test.c
@@ -206,6 +206,7 @@ int main () {
 	   versions of the frames */
 
 	// assume that MingW's shell can do redirection
+	system("md5sum *-sound.dump *-sound.raw > md5sums.txt");
 	system("git status --porcelain out > t.out");
 
 	const char *path= "t.out";
